4c6610454a2293096a650b6356b1ea79eb9c0d9c
William Munoz <william@rustdeveloper.io>
2024-02-23 20:09:55 -0500

    add context

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::net::SocketAddr;
 use axum::response::{Html, IntoResponse, Response};
-use axum::Router;
+use axum::{Json, Router};
 use axum::routing::{get, get_service};
 use axum::extract::{Path, Query};
 use axum::middleware;
 
 use serde::Deserialize;
+use serde_json::json;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 use crate::model::ModelController;
 pub use self::error::{Error, Result};
 
@@ -15,6 +17,7 @@ pub use self::error::{Error, Result};
 mod error;
 mod web;
 mod model;
+mod ctx;
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
@@ -34,6 +37,10 @@ async fn main() -> Result<()> {
         .merge(web::routes_login::routes())
         .nest("/api", routes_apis)
         .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(
+            mc.clone(),
+            web::mw_auth::mw_ctx_resolver,
+        ))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
 
@@ -49,8 +56,34 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("--> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    // -- Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // -- If client error, build the new response
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            // Build the new response from the client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // -- ToDo: Build and log the server log line
+    println!("  ->> server log line - {uuid} - Error: {service_error:?}" );
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,21 +1,92 @@
 //! src/web/mw_auth.rs
 
+use async_trait::async_trait;
+use axum::extract::{FromRequestParts, State};
 use axum::http::Request;
+use axum::http::request::Parts;
 use axum::middleware::Next;
+//use axum::RequestPartsExt;
 use axum::response::Response;
-use tower_cookies::Cookies;
+use lazy_regex::regex_captures;
+use tower_cookies::{Cookie, Cookies};
 use crate::web::AUTH_TOKEN;
 use crate::{Error, Result};
+use crate::ctx::Ctx;
+use crate::model::ModelController;
 
 pub async fn mw_require_auth<B>(
-    cookies: Cookies,
+    ctx: Result<Ctx>,
     req: Request<B>,
     next: Next<B>) -> Result<Response> {
+    println!("->> {:<12} - mv_require_auth -  {ctx:?}", "MIDDLEWARE");
+
+    ctx?;
+
+    Ok(next.run(req).await)
+}
+
+pub async fn mw_ctx_resolver<B>(
+    _mc: State<ModelController>,
+    cookies: Cookies,
+    mut req: Request<B>,
+    next: Next<B>,
+) -> Result<Response> {
+    println!("->> {:<12} - mw_ctx_resolver", "MIDDLEWARE");
 
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-    // ToDo: Real auth-token parsing & validation.
-    auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
+    // Compute Result<Ctx>
+    let result_ctx = match auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)
+    {
+        Ok((user_id, _exp, _sign)) => {
+            // ToDo: Token components validations
+            Ok(Ctx::new(user_id))
+        }
+        Err(e) => Err(e),
+    };
+
+    // Remove the cookie if something went wrong other than NoAuthTokenCookie
+    if result_ctx.is_err()
+        && !matches!(result_ctx, Err(Error::AuthFailNoAuthTokenCookie))
+    {
+        cookies.remove(Cookie::named(AUTH_TOKEN))
+    }
+
+    // Store the ctx_result in the request extension
+    req.extensions_mut().insert(result_ctx);
 
     Ok(next.run(req).await)
+}
+
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "Extractor");
+
+        parts
+            .extensions
+            .get::<Result<Ctx>>()
+            .ok_or(Error::AuthFailCtxNotInRequestExt)?
+            .clone()
+    }
+}
+
+/// Parse a token of format `user-[user-id].[expiration].[signature]
+/// Returns (user_id, expiration, signature)
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+    let (_whole, user_id, exp, sign) = regex_captures!(
+        r#"^user-(\d+)\.(.+)\.(.+)"#, // a literal regex
+        &token
+    )
+        .ok_or(Error::AuthFailTokenWrongFormat)?;
+
+    let user_id: u64 = user_id
+        .parse()
+        .map_err(|_| Error::AuthFailTokenWrongFormat)?;
+
+    Ok((user_id, exp.to_string(), sign.to_string()  ))
 }
